473c843577f7d3c5da32a89205deecae8674d2b6
TrentSisson <trent.c.sisson0@gmail.com>
2021-04-16 00:12:06 -0500

    created update functionalitly for employees,animals, customers, and locations

--- animals/request.py
+++ animals/request.py
@@ -4,21 +4,24 @@ ANIMALS = [
         "name": "Snickers",
         "species": "Dog",
         "locationId": 1,
-        "customerId": 4
+        "customerId": 4,
+        "status": "admitted"
     },
     {
         "id": 2,
         "name": "Gypsy",
         "species": "Dog",
-        "locationId": 1,
-        "customerId": 2
+        "location": 1,
+        "customerId": 2,
+        "status": "Admitted"
     },
     {
         "id": 3,
         "name": "Blue",
         "species": "Cat",
         "locationId": 2,
-        "customerId": 1
+        "customerId": 1,
+        "status": "admitted"
     }
 ]
 
@@ -60,6 +63,7 @@ def create_animal(animal):
     # Return the dictionary with `id` property added
     return animal
 
+
 def delete_animal(id):
     # Initial -1 value for animal index, in case one isn't found
     animal_index = -1
@@ -74,3 +78,12 @@ def delete_animal(id):
     # If the animal was found, use pop(int) to remove it from list
     if animal_index >= 0:
         ANIMALS.pop(animal_index)
+
+def update_animal(id, new_animal):
+    # Iterate the ANIMALS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Update the value.
+            ANIMALS[index] = new_animal
+            break
